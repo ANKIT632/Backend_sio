aad96f10b2e188c9d79aaf6c1c5f7a485aed5dfb
ankit gupta <ankitgupta17878@gmail.com>
2024-06-16 23:20:33 +0530

    -

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 import express from 'express';
 import { createServer } from 'node:http';
+import { fileURLToPath } from 'node:url';
+import { dirname, join } from 'node:path';
+import { Server } from 'socket.io';
 
 const app = express();
 const server = createServer(app);
+const io = new Server(server);
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello world</h1>');
+  res.sendFile(join(__dirname, 'index.html'));
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
 });
 
 server.listen(3000, () => {
   console.log('server running at http://localhost:3000');
 });
+
+
+// note
+
+// why use createServer instead of app.listen ?
+//  Easier integration with other Node.js core modules and third-party libraries

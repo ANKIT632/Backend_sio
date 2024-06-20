9db0db6fa43869b118bbcc333df0c9ef160b8402
ankit gupta <ankitgupta17878@gmail.com>
2024-06-20 14:51:57 +0530

    -

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ import { createServer } from 'node:http';
 import { fileURLToPath } from 'node:url';
 import { dirname, join } from 'node:path';
 import { Server } from 'socket.io';
-import os from 'os';
+// import os from 'os';
 
 const app = express();
 const server = createServer(app);
-const io = new Server(server);
+const io = new Server(server,{
+  connectionStateRecovery: {}
+});
 
 
  
